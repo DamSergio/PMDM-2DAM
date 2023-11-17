4d30a96e35f2c349a6e1de05646f8987bb017924
DamSergio <dam_sergioballesteros@riberadeltajo.es>
2023-11-17 15:58:17 +0100

    ShareMyBike

--- ShareMyBike/app/src/main/java/com/example/sharemybike/MainActivity.java
+++ ShareMyBike/app/src/main/java/com/example/sharemybike/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.sharemybike;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,5 +34,32 @@ public class MainActivity extends AppCompatActivity {
         });
 
         ImageButton btnMail = findViewById(R.id.btnMail);
+        btnMail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView txtMail = findViewById(R.id.txtMail);
+                String mail = txtMail.getText().toString();
+
+                Intent intentMail = new Intent(Intent.ACTION_SEND);
+                intentMail.setData(Uri.parse("mailto:"+mail));
+
+                intentMail.putExtra(Intent.EXTRA_EMAIL, mail);
+                intentMail.putExtra(Intent.EXTRA_SUBJECT, "Share My Bike");
+                intentMail.putExtra(Intent.EXTRA_TEXT, "");
+
+                intentMail.setType("message/rfc822");
+
+                startActivity(intentMail);
+            }
+        });
+
+        Button btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent bikeActivity = new Intent(getApplicationContext(), BikeActivity.class);
+                startActivity(bikeActivity);
+            }
+        });
     }
 }
